3c3d7cab4b857a8149679ba611a8a01fdd93a6dc
Florian Sassmann <florian.sassmann@hotmail.com>
2024-07-20 06:52:56 +0000

    add styling for cards and create an array

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,3 +36,76 @@ const scoreboard = document.getElementById("scoreboard");
 openScoreboard.addEventListener("click", function() {
     scoreboard.classList.add("open");
 });
+
+
+const cards = document.getElementsByClassName("card");
+
+let allMemory = document.getElementsByClassName("card-image");
+
+
+const memoryArray = [
+    {
+        id: 1,
+        image: document.getElementById("Master Chief image")
+    },
+    {
+        id: 2,
+        image: document.getElementById("Arbiter image")
+    },
+    {
+        id: 3,
+        image: document.getElementById("Cortana image")
+    },
+    {
+        id: 4,
+        image: document.getElementById("Avery Johnson image")
+    },
+    {
+        id: 5,
+        image: document.getElementById("Energy Sword image")
+    },
+    {
+        id: 6,
+        image: document.getElementById("Halo Installation image")
+    },
+    {
+        id: 7,
+        image: document.getElementById("Master Chief Helmet image")
+    },
+    {
+        id: 8,
+        image: document.getElementById("UNSC Logo image")
+    },
+    {
+        id: 9,
+        image: document.getElementById("Master Chief image")
+    },
+    {
+        id: 10,
+        image: document.getElementById("Arbiter image")
+    },
+    {
+        id: 11,
+        image: document.getElementById("Cortana image")
+    },
+    {
+        id: 12,
+        image: document.getElementById("Avery Johnson image")
+    },
+    {
+        id: 13,
+        image: document.getElementById("Energy Sword image")
+    },
+    {
+        id: 14,
+        image: document.getElementById("Halo Installation image")
+    },
+    {
+        id: 15,
+        image: document.getElementById("Master Chief Helmet image")
+    },
+    {
+        id: 16,
+        image: document.getElementById("UNSC Logo image")
+    }
+]
